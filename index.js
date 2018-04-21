0a06c63020a898579fc6a6fcb97ec16697c6ea22
Robin van der Vleuten <robin@webstronauts.co>
2018-04-21 13:59:38 +0200

    Improved plugin resolving

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-module.exports = function (config, env, helpers) {
-  const { rule } = helpers.getLoadersByName(config, 'babel-loader')[0];
-  const babelConfig = rule.options;
-
-  babelConfig.plugins.push('babel-plugin-styled-components');
+module.exports = function (config) {
+  const babel = config.module.loaders.filter(loader => loader.loader === 'babel-loader')[0].options;
+  babel.plugins.push(require.resolve('babel-plugin-styled-components'));
 };
